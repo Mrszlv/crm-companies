c3795fd94a7dd8220d38b2282cae5f2799cc852e
Miroszlav <miroszlav.popovics@gmail.com>
2025-03-27 00:16:33 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import { headers } from 'next/headers';
 import AddCompanyButton from './components/add-company-button';
 
 export default function Home() {
-  console.log(headers());
-
   return (
     <main>
       <h1 className="text-xl">Home page{new Date().toTimeString()}</h1>
