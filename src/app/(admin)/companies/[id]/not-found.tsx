9405033aec4c58790c0a01c4b075c221fc2958a3
Miroszlav <miroszlav.popovics@gmail.com>
2025-04-16 18:18:41 +0300

    parallel-routes

--- src/app/(admin)/companies/[id]/not-found.tsx
+++ src/app/(admin)/companies/[id]/not-found.tsx
@@ -7,8 +7,10 @@ import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <div>
-      <p>No such company found</p>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <p>
+        <span className="text-red-600">404 ERROR</span> No such company found
+      </p>
       <Link href="/companies" className="text-blue-400">
         Back to companies
       </Link>
--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,5 +1,5 @@
 import Header from '@/app/components/header';
-import MagicButton from '@/app/components/magic-button';
+
 import React from 'react';
 
 // export interface PageProps {}
@@ -8,9 +8,6 @@ export default function Page() {
   return (
     <main>
       <Header>Dashboard</Header>
-      <div className="mt-5 ml-10">
-        <MagicButton />
-      </div>
     </main>
   );
 }
