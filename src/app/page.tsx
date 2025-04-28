66001793115da541f28dbc51e8b0703357ce833e
Miroszlav <miroszlav.popovics@gmail.com>
2025-04-28 19:02:15 +0300

    update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import AddCompanyButton from './components/add-company-button';
-import Header from './components/header';
-
 export default function Home() {
   return (
     <main>
-      <Header>Home page</Header>
-      <div className="flex gap-5 mt-5 ml-10">
-        <AddCompanyButton />
-      </div>
+      <h1 className="text-xl">Home page</h1>
     </main>
   );
 }
